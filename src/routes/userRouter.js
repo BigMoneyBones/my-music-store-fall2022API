69e821040626524d385f33d2fbfb96b12a13468d
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-10-06 13:56:53 -0500

    first commit, thursday

---
+++ src/routes/userRouter.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const userRouter = express.Router();
+
+// Route to create user accounts
+userRouter.post("/register-user", (req, res, next) => {
+  //   console.log("Route is working");
+  //   console.log('req: ', req)
+  console.log("req.body: ", req.body);
+
+  res.send("Route hit successfully");
+});
+
+module.exports = userRouter;
