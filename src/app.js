5accf8e28e7d5b4164285ce352f951e4060d443c
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-10-07 16:30:26 -0500

    connected to mongo db

--- src/app.js
+++ src/app.js
@@ -1,44 +1,31 @@
 require("dotenv").config();
-const express = require("express");
 const bodyParser = require("body-parser");
+const express = require("express");
 const userRouter = require("./routes/userRouter");
+const mongoose = require("mongoose");
+
+// server port
 const port = process.env.PORT;
 
 // Now app is the express server.
 const app = express();
 
-// parsing JSON to req.body
+mongoose
+  .connect(process.env.MONGO_DB_CONNECTION_STRING)
+  .then(() => console.log("Connected to MONGO DB successfully"))
+  .catch(() => console.log("Error connecting to MONGO DB"));
+
+// Parsing JSON to req.body
 app.use(bodyParser.json());
 
 //Express Middlewares:
 // A function that runs in the middle of our request.
-// That a function that runs after the server recieves a request and before the express server sends a response.
-// console.log("hey");
+// A function that runs after the server recieves a request and before the express server sends a response.
 
 // userRoutes
+app.use(userRouter);
 
+// // what port you want the app to "listen" to, second arg is callback function to run
 app.listen(port, () =>
   console.log("Music store server is listening for request")
 );
-
-// Auth User
-
-// app.use((req, res, next) => {
-//   console.log("Pretend user is being verified");
-
-//   next();
-// });
-
-// app.get("/home", (req, res, next) => {
-//   res.send("Home Page");
-//   next();
-// });
-
-// app.post("/user", (req, res, next) => {
-//   res.send("responding to user route");
-// });
-
-// // what port you want the app to "listen" to, second arg is callback function to run
-// app.listen(3010, () =>
-//   console.log("Music store server is listening for request")
-// );
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,7 +4,7 @@ const userRouter = express.Router();
 
 // Route to create user accounts
 userRouter.post("/register-user", (req, res, next) => {
-  //   console.log("Route is working");
+  // console.log("Route is working");
   //   console.log('req: ', req)
   console.log("req.body: ", req.body);
 
