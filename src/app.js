1146e4c8f867e122842f793a6393ead98cb80755
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-10-12 22:59:27 -0500

    compared, fixed, and updated with code from class

--- src/app.js
+++ src/app.js
@@ -1,59 +1,31 @@
 require("dotenv").config();
 const bodyParser = require("body-parser");
+var cors = require("cors");
 const express = require("express");
-const userRouter = require("./routes/userRouter");
 const mongoose = require("mongoose");
-var cors = require('cors')
+const userRouter = require("./routes/userRouter");
 
-// server port
 const port = process.env.PORT;
 
-// Now app is the express server.
 const app = express();
 
 mongoose
   .connect(process.env.MONGO_DB_CONNECTION_STRING)
-  .then(() => console.log("Connected to MONGO DB successfully"))
-  .catch(() => console.log("Error connecting to MONGO DB"));
+  .then(() => console.log("Connected to mongo db successfully"))
+  .catch(() => console.log("unable to connect to mongodb"));
 
-  app.use(cors())
+app.use(cors());
 
 // Parsing JSON to req.body
 app.use(bodyParser.json());
 
-//Express Middlewares:
-// A function that runs in the middle of our request.
-// A function that runs after the server recieves a request and before the express server sends a response.
+// EXPRESS MIDDLEWARES:
+// A function that run in the middle of our request.
+// That a function that runs after the server receives a request and before the express server sends a response.
 
-// userRoutes
+//userRoutes
 app.use(userRouter);
 
-// // what port you want the app to "listen" to, second arg is callback function to run
 app.listen(port, () =>
   console.log("Music store server is listening for request")
 );
-
-
-app.use(error)
-
-
-
-
-// // Error middleware
-//   // 1st arg => error
-// const errorHandler = (error, req, res, next) => {
-  
-//   if (res.headersSent) {
-//     return next(err)
-//   }
-
-//     // if(error.message === "cant connect to database"){
-//     //   // send and email to seniorDevs on team
-//     //   // wake developers to fix issue
-//     //   console.log('Error: ', error)
-//     // }
-
-//   res.status(500).send('Colossal Error')
-// }
-
-// app.use(errorHandler)
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
-const userRouter = require("../routes/userRouter");
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true, trim: true },
   lastName: { type: String, required: true, trim: true },
-  email: { type: String, required: true, trim: true, unique: true }, // unique property to only allow 1 account per email
-  profilePicture: { type: String, required: true, trim: true },
+  email: { type: String, required: true, trim: true, unique: true },
   hashedPassword: { type: String, required: true },
+  profilePicture: { type: String, required: true, trim: true },
   isAdmin: { type: Boolean, required: true, default: false },
 });
 
-const UserModel = mongoose.model("user", userSchema);
+const UserModel = mongoose.model("User", userSchema);
 
 module.exports = UserModel;
