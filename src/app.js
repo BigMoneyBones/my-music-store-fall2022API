a0f5a006e35931aeed30de6a15c006ea9e1ef624
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-10-19 15:02:38 -0500

    everything working..?

--- src/app.js
+++ src/app.js
@@ -1,23 +1,65 @@
-require("dotenv").config();
-const bodyParser = require("body-parser");
-var cors = require("cors");
-const express = require("express");
-const mongoose = require("mongoose");
-const userRouter = require("./routes/userRouter");
+require('dotenv').config()
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+var cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const UserModel = require('./models/UserModel');
+const userRouter = require('./routes/userRouter');
+const jwt = require('jsonwebtoken');
+const productRouter = require('./routes/productRouter');
+
 
 const port = process.env.PORT;
 
 const app = express();
 
-mongoose
-  .connect(process.env.MONGO_DB_CONNECTION_STRING)
-  .then(() => console.log("Connected to mongo db successfully"))
-  .catch(() => console.log("unable to connect to mongodb"));
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING)
+.then(() => console.log("Connected to mongo db successfully"))
+.catch(() => console.log("unable to connect to mongodb"))
 
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:3000'
+}));
+
+app.use(cookieParser());
 
 // Parsing JSON to req.body
 app.use(bodyParser.json());
+
+//Authorization Middleware
+app.use(async (req, res, next) => {
+  try {
+      const { session_token: sessionToken } = req.cookies;
+      
+      if(!sessionToken){
+        return next();
+      }
+    
+      // This returns the data in the JWT or it throws if the jwt is not valid
+      const { userId, iat } = jwt.verify(sessionToken, process.env.AUTH_SECRET_KEY);
+    
+      // if the token is too old we reject it.
+      if( iat < (Date.now() - (30 * 24 * 60 * 60 * 1000))){
+        return res.status(401).send("Session expired")
+      }
+    
+      const foundUser = await UserModel.findOne( { _id: userId }); 
+      
+      if(!foundUser){
+        return next();
+      }
+    
+      // after we find the user in the token we add it to the request
+      req.user = foundUser;
+    
+      return next();
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 // EXPRESS MIDDLEWARES:
 // A function that run in the middle of our request.
@@ -26,6 +68,8 @@ app.use(bodyParser.json());
 //userRoutes
 app.use(userRouter);
 
-app.listen(port, () =>
-  console.log("Music store server is listening for request")
-);
+// Product Routes
+app.use(productRouter);
+
+
+app.listen(port, () => console.log('Music store server is listening for request'));
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,44 +1,16 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
-const UserModel = require("../models/UserModel");
+const express = require('express');
+const UserService = require('../Services/UserService');
 
 const userRouter = express.Router();
 
+userRouter.get('/sign-out', UserService.signOut)
+userRouter.post('/sign-in', UserService.signIn);
+
 // Route to create user accounts
-userRouter.post("/register-user", async (req, res, next) => {
-  // save the user information in the database
+userRouter.post('/register-user', UserService.registerUser);
 
-  // get credentials and user info from the front end
-  const { firstName, lastName, email, password, profilePicture } = req.body;
+userRouter.post('/update-favorites', UserService.updateFavorites);
 
-  // HASH (Bcrypt) the password
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  try {
-    // store a password hash, not the password
-    const userDocument = new UserModel({
-      firstName,
-      lastName,
-      email,
-      hashedPassword,
-      profilePicture: 17,
-    });
-
-    await userDocument.save();
-
-    res.send({
-      user: {
-        id: userDocument._id,
-        firstName,
-        lastName,
-        email,
-        profilePicture,
-        isAdmin: userDocument.isAdmin,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+userRouter.get('/user-favorites', UserService.getUserFavorites);
 
 module.exports = userRouter;
